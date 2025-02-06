36cb122de90163ee4de38e4a37e57fd039d29fb8
somNaut7 <74354972+SomilNautiyal@users.noreply.github.com>
2025-02-07 01:45:49 +0530

    Uploading src

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/SomilNautiyal/webflix
+
+go 1.23.4
---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "static/Webflix.html")
+}
+
+func moviesPage(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "static/index.html")
+}
+
+func signinPage(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "static/signin.html")
+}
+
+func contactPage(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "static/Contact Us.html")
+}
+
+func main() {
+
+	http.HandleFunc("/home", homePage)
+	http.HandleFunc("/movies", moviesPage)
+	http.HandleFunc("/about", aboutPage)
+	http.HandleFunc("/contact", contactPage)
+
+	err := http.ListenAndServe("0.0.0.0:8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
