3020006ee066c6be4dff102b5970d4034ac3fd2d
Somil Nautiyal <somilnautiyal118@gmail.com>
2025-02-16 14:39:54 +0530

    Testing changes

--- main.go
+++ main.go
@@ -11,12 +11,12 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 
-	fs := http.FileServer(http.Dir("static"))
+	fs := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fs)
 
 	http.HandleFunc("/home", homePage)
 
-	err := http.ListenAndServe("0.0.0.0:8080", nil)
+	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
