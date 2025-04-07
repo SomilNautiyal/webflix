cabdfedda83b7b5aab8b6281c1ee187d5af68b9d
Somil Nautiyal <somilnautiyal118@gmail.com>
2025-04-07 15:10:18 +0530

    Optimizations

--- static/main.js
+++ static/main.js
@@ -1,51 +1,43 @@
 (function ($) {
-    ("use strict");
-  
-    // closes the sidebar menu
-    $(".menu-toggle").click(function (e) {
-      e.preventDefault();
-      $("#sidebar-wrapper").toggleClass("active");
-      $(".menu-toggle > .fa-bars, .menu-toggle > .fa-times").toggleClass(
-        "fa-bars fa-times"
-      );
-      $(this).toggleClass("active");
-    });
-  
-    //smooth scrolling using jquery and easing
-    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
-      if (
-        location.pathname.replace(/^\//, "") ==
-          this.pathname.replace(/^\//, "") &&
-        location.hostname == this.hostname
-      ) {
-        var target = $(this.hash);
-        target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
-        if (target.length) {
-          $("html, body").animate(
-            {
-              scrollTop: target.offset().top,
-            },
-            1000,
-            "easeInOutExpo"
-          );
-          return false;
-        }
+  "use strict";
+
+  // Toggle sidebar menu
+  $(".menu-toggle").on("click", function (e) {
+    e.preventDefault();
+    $("#sidebar-wrapper").toggleClass("active");
+    $(this).toggleClass("active");
+    $(".menu-toggle > .fa-bars, .menu-toggle > .fa-times").toggleClass("fa-bars fa-times");
+  });
+
+  // Smooth scrolling for anchor links
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').on("click", function () {
+    const { pathname, hostname, hash } = this;
+    if (
+      location.pathname.replace(/^\//, "") === pathname.replace(/^\//, "") &&
+      location.hostname === hostname
+    ) {
+      let target = $(hash);
+      target = target.length ? target : $("[name=" + hash.slice(1) + "]");
+      if (target.length) {
+        $("html, body").animate(
+          { scrollTop: target.offset().top },
+          1000,
+          "easeInOutExpo"
+        );
+        return false;
       }
-    });
-    $("#sidebar-wrapper .js-scroll-trigger").click(function () {
-      $("#sidebar-wrapper").removeClass("active");
-      $("menu-toggle").removeClass("active");
-      $(".menu-toggle > fa.bars, .menu-toggle > .fa-times").toggleClass(
-        "fa-bars fa-times"
-      );
-    });
-  
-    $(document).scroll(function () {
-      var scrollDistance = $(this).scrollTop();
-      if (scrollDistance > 100) {
-        $(".scroll-to-top").fadeIn();
-      } else {
-        $(".scroll-to-top").fadeOut();
-      }
-    });
-  })(jQuery);
+    }
+  });
+
+  // Close sidebar when a scroll trigger link is clicked
+  $("#sidebar-wrapper .js-scroll-trigger").on("click", function () {
+    $("#sidebar-wrapper").removeClass("active");
+    $(".menu-toggle").removeClass("active");
+    $(".menu-toggle > .fa-bars, .menu-toggle > .fa-times").toggleClass("fa-bars fa-times");
+  });
+
+  // Show/hide scroll-to-top button on scroll
+  $(document).on("scroll", function () {
+    $(".scroll-to-top").fadeToggle($(this).scrollTop() > 100);
+  });
+})(jQuery);
